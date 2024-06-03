795c14466e38e3e6f3be469ff1bdef535d456e8b
PedroAdair <pedro.adair.avila@gmail.com>
2024-06-03 16:56:06 -0600

    v 2.4.6 | A1 & A2

--- API/Rentabilidad/Exp2/func2.py
+++ API/Rentabilidad/Exp2/func2.py
@@ -6,11 +6,18 @@ from dotenv import load_dotenv
 load_dotenv('venv/.env')
 import os
 import json
-import numpy
+import numpy as np
+
+
+
 
 with open('DB_examples/primerinputProductos.json', 'r') as f:
 
     products =  json.loads(f.read())
+
+with open('DB_examples/primerinputPagos.json', 'r') as f:
+
+    payments =  json.loads(f.read())
 
 print(products)
 
@@ -42,7 +49,8 @@ def getCompleteDatosPedimiento(products:dict):
 
     return products
 
-######################################################################################
+############################################################################################################################################################################
+############################################################################################################################################################################
 
 
 def getTotalImporteUSD(products:dict):
@@ -51,20 +59,69 @@ def getTotalImporteUSD(products:dict):
 
     totales_importeUSD = sum(map(lambda x: x["A1"]["importeUSD"], products["productos"]))
 
-    products["importeUSD"] = totales_importeUSD
+    products['totales']["importeUSD"] = totales_importeUSD
     return products
 
+#############################################################################################################################################################################
+#A1
+#############################################################################################################################################################################
+def obtenerA1(payments:dict, products:dict):
+    #obtener el tipo de cambio
+    tipoCambio = payments['tipoCambio']['globales']['tipoCambio']
+    #optener el precio en moneda mexicana considerando el tipo de cambio por la compra
+    importesUSD = lambda x: {**x, 'A1': {**x['A1'], 'importeMN': round(x['A1']['importeUSD']*tipoCambio,2)}}
+    products['productos'] = list(map(importesUSD, products["productos"]))
 
+    totales_importeMN = sum(map(lambda x: x["A1"]["importeMN"], products["productos"]))
+    products['totales'].update({'importeMN':totales_importeMN}) 
+    return products 
+
+#############################################################################################################################################################################
+#A2
+#############################################################################################################################################################################
+def obtenerA2(payments:dict, products:dict):
+    promedioMN = payments['tipoCambio']['globales']['MN']
+    totalUnidades = products['totales']['unidades']
+    #despachoAduanalMN
+    despachosAduanalMN = lambda x: {**x, 'A2': {**x['A2'], 'despachoAduanalMN': round(promedioMN/totalUnidades*x['datosPedimiento']['unidades'],2)}}
+    products['productos'] = list(map(despachosAduanalMN, products["productos"]))
+    #despachoAduanalMNUnit
+    despachosAduanalMNUnit = lambda x: {**x, 'A2': {**x['A2'], 'despachoAduanalMNUnit': round(promedioMN/totalUnidades,2)}}
+    products['productos'] = list(map(despachosAduanalMNUnit, products["productos"]))
+    #Obtener totales de despachoAduanal
+    totales_despachoAduanalMN = round(sum(map(lambda x: x["A2"]["despachoAduanalMN"], products["productos"])),2)
+    products['totales']["despachoAduanalMN"] = totales_despachoAduanalMN
+    return products
+
+############################################################################################################################################################################
+# Pagos
+############################################################################################################################################################################
+def registrarPrimerPago(payments:dict):
+    tipoCambio = round(np.mean(np.array(list(map(lambda x: x["tipoCambio"], payments["tipoCambio"]["historial"])))),3)
+    payments['tipoCambio']['globales']['tipoCambio'] = tipoCambio
+    return payments
+
+
+#############################################################################################################################################################################
+# Proceso
+#############################################################################################################################################################################
 print("*"*150)
+#paso 1. registrar los productos con su primeras caracteristicas
+#es posible unir esras funciones en una sola
 products = getCompleteDatosPedimiento(products=products)
 products = getTotalImporteUSD(products=products)
+#paso 2. registrar el primer pago
+payments = registrarPrimerPago(payments=payments)
+#Paso 3
+products = obtenerA1(payments=payments, products=products)
+#paso 4 Obtener A2
+products = obtenerA2(payments=payments ,products=products)
 
 
-print(products)
-# print(list(importeUSD))
-# products = getTotalImporteUSA(products=products)
-
-# # print(products["productos"])
-# for product in products['productos']:
-#     print(product["A1"])
+ 
+print(products['productos'][0]) 
+print("**"*100)
+print(products['totales'])     
+# aa = list(map(lambda x: x['A1'].update({'importeMN': x['A1']['importeUSD']*tipoCambio}), products["productos"]))
+# print(aa)
 
